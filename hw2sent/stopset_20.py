ec31c9a7fb122bc1425b4a1bb6d5b5eaea302083
eldorz <ldawes@gmail.com>
2017-09-30 09:21:30 +1000

    range of stopsets

---
+++ hw2sent/stopset_20.py
@@ -0,0 +1,4 @@
+stops = {'the', 'and', 'a', 'of', 'to', 'is', 'in', 'it', 'i', 'this', 'that',
+	'br', 'was', 'as', 'for', 'with', 'but', 'on', 'you', 'are'}
+
+
---
+++ hw2sent/stopset_30.py
@@ -0,0 +1,5 @@
+stops = {'the', 'and', 'a', 'of', 'to', 'is', 'in', 'it', 'i', 'this',
+	'that', 'br', 'was', 'as', 'for', 'with', 'but', 'on', 'you', 'are',
+	'his', 'have', 'he', 'be', 'one', 'its', 'at', 'all', 'by', 'an'}
+
+
